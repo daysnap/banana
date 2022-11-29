c92ad7c2bcaf8426570a949bb6a5a318a0cd7574
chenzhigang <979703986@qq.com>
2022-11-29 16:52:45 +0800

    chore: 入口函数

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
+import { assignment } from './assignment'
+import { extract } from './extract'
+import { validate } from './validate'
+
 export * from './assignment'
 export * from './extract'
 export * from './type'
 export * from './validate'
+
+export const banana = {
+  assignment,
+  extract,
+  validate,
+}
+
+export default banana
